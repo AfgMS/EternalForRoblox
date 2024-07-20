158348bb2f01c9a893ba8a0b5a306ee8d2443e58
nothm_ <127741557+AfgMS@users.noreply.github.com>
2024-07-20 08:35:28 +0700

    Update loader.lua
    BridgeDuels Support!

--- Eternal/loader.lua
+++ Eternal/loader.lua
@@ -3,6 +3,7 @@ local MainFolder = "Eternal"
 local ConfigFolder = MainFolder .. "/config"
 local LogsFolder = MainFolder .. "/logs"
 local LibraryURL = "https://raw.githubusercontent.com/AfgMS/EternalForRoblox/main/Eternal/library.lua"
+local BridgeDuelsURL = "https://raw.githubusercontent.com/AfgMS/EternalForRoblox/main/Eternal/config/BridgeDuels.lua"
 local UniversalURL = "https://raw.githubusercontent.com/AfgMS/EternalForRoblox/main/Eternal/config/Universal.lua"
 
 local function GetGithub(url, path)
@@ -23,6 +24,8 @@ if isfolder(MainFolder) and isfolder(ConfigFolder) and isfolder(LogsFolder) then
 	
 	if game.PlaceId == 6872274481 or game.PlaceId == 8560631822 or game.PlaceId == 8444591321 then
 		writefile(LogsFolder .. "/error_" .. game.PlaceId .. ".txt", "This game is not supported by Eternal")
+	elseif game.PlaceId == 11630038968 then
+		loadstring(game:HttpGet(BridgeDuelsURL))()
 	else
 		loadstring(game:HttpGet(UniversalURL))()
 	end
