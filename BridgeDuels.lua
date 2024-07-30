6ad1334c2fe5429361cb2a9b5c3a78d440307a2a
nothm_ <127741557+AfgMS@users.noreply.github.com>
2024-07-30 20:44:33 +0700

    Update BridgeDuels.lua
    Changelogs:
    -Modified TargetStrafe (should works now)
    -Scaffold Fix (part 5 grr)

--- BridgeDuels.lua
+++ BridgeDuels.lua
@@ -148,12 +148,13 @@ spawn(function()
 			if callback then
 				repeat
 					task.wait()
-					if not IsAlive(LocalPlayer) then repeat task.wait() until IsAlive(LocalPlayer) end
-					local NearestPlayer = GetNearestPlayer(30)
-					if NearestPlayer then
-						for i, v in pairs(LocalPlayer.Backpack:GetChildren()) do
-							if v:IsA("Tool") and v.Name:match("Sword") then
-								Humanoid:EquipTool(v)
+					if IsAlive(LocalPlayer) then
+						local NearestPlayer = GetNearestPlayer(30)
+						if NearestPlayer then
+							for i, v in pairs(LocalPlayer.Backpack:GetChildren()) do
+								if v:IsA("Tool") and v.Name:match("Sword") then
+									Humanoid:EquipTool(v)
+								end
 							end
 						end
 					end
@@ -187,47 +188,48 @@ spawn(function()
 			if callback then
 				repeat
 					task.wait()
-					if not IsAlive(LocalPlayer) then repeat task.wait() until IsAlive(LocalPlayer) end
-					local NearestPlayer = GetNearestPlayer(Range)
-					if NearestPlayer then
-						local Sword = GetTool("Sword")
-						if Sword then
-							BlockAnim = Sword:WaitForChild("Animations").BlockHit
-							SwingAnim = Sword:WaitForChild("Animations").Swing
-							if Swing then
-								Humanoid:LoadAnimation(SwingAnim):Play()
+					if IsAlive(LocalPlayer) then
+						local NearestPlayer = GetNearestPlayer(Range)
+						if NearestPlayer then
+							local Sword = GetTool("Sword")
+							if Sword then
+								BlockAnim = Sword:WaitForChild("Animations").BlockHit
+								SwingAnim = Sword:WaitForChild("Animations").Swing
+								if Swing then
+									Humanoid:LoadAnimation(SwingAnim):Play()
+								end
+								if Mode == "Fake" then
+									Humanoid:LoadAnimation(BlockAnim):Play()
+									local args = {
+										[1] = false,
+										[2] = Sword.Name
+									}
+
+									game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("ToolService"):WaitForChild("RF"):WaitForChild("ToggleBlockSword"):InvokeServer(unpack(args))
+								elseif Mode == "Remote" then
+									Humanoid:LoadAnimation(BlockAnim):Stop()
+									local args = {
+										[1] = true,
+										[2] = Sword.Name
+									}
+
+									game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("ToolService"):WaitForChild("RF"):WaitForChild("ToggleBlockSword"):InvokeServer(unpack(args))
+								elseif Mode == "Legit" then
+									Humanoid:LoadAnimation(BlockAnim):Play()
+									local args = {
+										[1] = true,
+										[2] = Sword.Name
+									}
+
+									game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("ToolService"):WaitForChild("RF"):WaitForChild("ToggleBlockSword"):InvokeServer(unpack(args))
+								end
+								local args = {
+									[1] = NearestPlayer.Character,
+									[2] = KillAuraCrit,
+									[3] = Sword.Name
+								}
+								game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("ToolService"):WaitForChild("RF"):WaitForChild("AttackPlayerWithSword"):InvokeServer(unpack(args))
 							end
-							if Mode == "Fake" then
-								Humanoid:LoadAnimation(BlockAnim):Play()
-								local args = {
-									[1] = false,
-									[2] = Sword.Name
-								}
-
-								game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("ToolService"):WaitForChild("RF"):WaitForChild("ToggleBlockSword"):InvokeServer(unpack(args))
-							elseif Mode == "Remote" then
-								Humanoid:LoadAnimation(BlockAnim):Stop()
-								local args = {
-									[1] = true,
-									[2] = Sword.Name
-								}
-
-								game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("ToolService"):WaitForChild("RF"):WaitForChild("ToggleBlockSword"):InvokeServer(unpack(args))
-							elseif Mode == "Legit" then
-								Humanoid:LoadAnimation(BlockAnim):Play()
-								local args = {
-									[1] = true,
-									[2] = Sword.Name
-								}
-
-								game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("ToolService"):WaitForChild("RF"):WaitForChild("ToggleBlockSword"):InvokeServer(unpack(args))
-							end
-							local args = {
-								[1] = NearestPlayer.Character,
-								[2] = KillAuraCrit,
-								[3] = Sword.Name
-							}
-							game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("ToolService"):WaitForChild("RF"):WaitForChild("AttackPlayerWithSword"):InvokeServer(unpack(args))
 						end
 					end
 				until not Enabled
@@ -387,37 +389,50 @@ spawn(function()
 end)
 
 spawn(function()
-	local OriginPos = Vector3.new(0, 0, 0)
+	local HumanoidRootPart = LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+	local Enabled, Diagonal, Expand = false, false, true
+	local DefaultPos = Vector3.zero
 	local function GetPlacePos(pos, diagonalmode)
-		local SelfPos = Vector3.new(math.floor((pos.X / 3) + 0.3) * 3, math.floor((pos.Y / 3) + 0.3) * 3, math.floor((pos.Z / 3) + 0.3) * 3)
-		local Offsets = (OriginPos - SelfPos)
+		local NewPos = Vector3.new(math.floor((pos.X / 3) + 0.5) * 3, math.floor((pos.Y / 3) + 0.5) * 3, math.floor((pos.Z / 3) + 0.5) * 3)
+		local Offset = (DefaultPos - NewPos)
 		if IsAlive(LocalPlayer) then
 			local HumanoidAngle = math.deg(math.atan2(-Humanoid.MoveDirection.X, -Humanoid.MoveDirection.Z))
-			local DiagonalPos = (HumanoidAngle >= 130 and HumanoidAngle <= 150) or (HumanoidAngle <= -35 and HumanoidAngle >= -50) or (HumanoidAngle >= 35 and HumanoidAngle <= 50) or (HumanoidAngle <= -130 and HumanoidAngle >= -150)
-			if DiagonalPos and ((Offsets.X == 0 and Offsets.Z ~= 0) or (Offsets.X ~= 0 and Offsets.Z == 0)) and diagonalmode then
-				return OriginPos
+			local DiagonalCheck = (HumanoidAngle >= 130 and HumanoidAngle <= 150) or (HumanoidAngle <= -35 and HumanoidAngle >= -50) or (HumanoidAngle >= 35 and HumanoidAngle <= 50) or (HumanoidAngle <= -130 and HumanoidAngle >= -150)
+			if DiagonalCheck and ((NewPos.X == 0 and NewPos.Z ~= 0) or (NewPos.X ~= 0 and NewPos.Z == 0)) and diagonalmode then
+				return DefaultPos
 			end
 		end
-		OriginPos = SelfPos
-		return SelfPos
+		return NewPos
 	end
-
-	local Enabled = false
+	
 	local Scaffold = Tabs.Movement:CreateToggle({
 		Name = "Scaffold",
 		Callback = function(callback)
 			Enabled = callback
 			if callback then
 				repeat
-					wait()
-					if not IsAlive(LocalPlayer) then repeat task.wait() until IsAlive(LocalPlayer) end
-					local PlacePos = GetPlacePos(LocalPlayer.Character:FindFirstChild("Head").Position + Vector3.new(1, -math.floor(Humanoid.HipHeight * 3), 0) + Humanoid.MoveDirection)
-					local args = {
-						[1] = PlacePos
-					}
+					task.wait()
+					if IsAlive(LocalPlayer) then
+						local Towers = UserInputService:IsKeyDown(Enum.KeyCode.Space) and UserInputService:GetFocusedTextBox() == nil
+						if Towers then
+							HumanoidRootPart.Velocity = Vector3.new(HumanoidRootPart.Velocity.X, 28, HumanoidRootPart.Velocity.Z)
+						end
+						for i = 1, Expand do
+							local PlacePos = GetPlacePos((HumanoidRootPart.Position + (Humanoid.MoveDirection * (i * 3.5))) + Vector3.new(0, -((HumanoidRootPart.Size.Y / 2) + Humanoid.HipHeight + 1.5)), 0)
+							PlacePos = Vector3.new(PlacePos.X, PlacePos.Y - (Towers and 4 or 0), PlacePos.Z)
+							local args = {
+								[1] = PlacePos
+							}
 
-					game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("ToolService"):WaitForChild("RF"):WaitForChild("PlaceBlock"):InvokeServer(unpack(args))
+							game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("ToolService"):WaitForChild("RF"):WaitForChild("PlaceBlock"):InvokeServer(unpack(args))
+							if Expand > 1 then
+								task.wait()
+							end
+							DefaultPos = PlacePos
+						end
+					end
 				until not Enabled
+				DefaultPos = Vector3.zero
 			end
 		end
 	})
@@ -435,24 +450,25 @@ spawn(function()
 			Enabled = callback
 			if callback then
 				repeat
-					wait()
-					if not IsAlive(LocalPlayer) then repeat task.wait() until IsAlive(LocalPlayer) end
-					if Mode == "WalkSpeed" then
-						Humanoid.JumpHeight = OldJumpHeight
-						Humanoid.WalkSpeed = NewSpeed
-					elseif Mode == "Hop" then
-						game.Workspace.Gravity = 82
-						Humanoid.WalkSpeed = OldWalkspeed
-						local velo = LocalPlayer.Character.Humanoid.MoveDirection * NewSpeed
-						HumanoidRootPart.Velocity = Vector3.new(velo.X, HumanoidRootPart.Velocity.Y, velo.Z)
-						Humanoid.Jump = true
-					elseif Mode == "LowHop" then
-						Humanoid.WalkSpeed = OldWalkspeed
-						game.Workspace.Gravity = OldGravity
-						Humanoid.JumpHeight = 0.20
-						local velo = LocalPlayer.Character.Humanoid.MoveDirection * (NewSpeed + 15)
-						HumanoidRootPart.Velocity = Vector3.new(velo.X, HumanoidRootPart.Velocity.Y, velo.Z)
-						Humanoid.Jump = true
+					task.wait()
+					if IsAlive(LocalPlayer) then
+						if Mode == "WalkSpeed" then
+							Humanoid.JumpHeight = OldJumpHeight
+							Humanoid.WalkSpeed = NewSpeed
+						elseif Mode == "Hop" then
+							game.Workspace.Gravity = 82
+							Humanoid.WalkSpeed = OldWalkspeed
+							local velo = LocalPlayer.Character.Humanoid.MoveDirection * NewSpeed
+							HumanoidRootPart.Velocity = Vector3.new(velo.X, HumanoidRootPart.Velocity.Y, velo.Z)
+							Humanoid.Jump = true
+						elseif Mode == "LowHop" then
+							Humanoid.WalkSpeed = OldWalkspeed
+							game.Workspace.Gravity = OldGravity
+							Humanoid.JumpHeight = 0.20
+							local velo = LocalPlayer.Character.Humanoid.MoveDirection * (NewSpeed + 15)
+							HumanoidRootPart.Velocity = Vector3.new(velo.X, HumanoidRootPart.Velocity.Y, velo.Z)
+							Humanoid.Jump = true
+						end
 					end
 				until not Enabled
 				Humanoid.WalkSpeed = OldWalkspeed
@@ -499,7 +515,6 @@ spawn(function()
 end)
 
 spawn(function()
-	local HumanoidRootPart = LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
 	local Enabled, Radius, Speed, Angle = false, nil, nil, 0
 	local TargetStrafe = Tabs.Movement:CreateToggle({
 		Name = "TargetStrafe",
@@ -510,11 +525,11 @@ spawn(function()
 					wait()
 					local NearestPlayer = GetNearestPlayer(28)
 					if NearestPlayer then
-						Angle += Speed
-						local Offsets = Vector3.new(math.cos(math.rad(Angle)), 0, math.sin(math.rad(Angle))) * Radius
-						local NewPos = NearestPlayer.Character.PrimaryPart.Position + Offsets
-						local velo = LocalPlayer.Character.Humanoid.MoveDirection * Speed
-						HumanoidRootPart.Velocity = Vector3.new(velo.X, HumanoidRootPart.Velocity.Y, velo.Z)
+						local DeltaTime = wait()
+						Angle = Angle + Speed * DeltaTime
+						local XPos = math.cos(Angle) * Radius
+						local ZPos = math.sin(Angle) * Radius
+						local NewPos = NearestPlayer.Character:FindFirstChild("HumanoidRootPart").Position + Vector3.new(XPos, 0, ZPos)
 						Humanoid:MoveTo(NewPos)
 					end
 				until not Enabled
@@ -525,7 +540,7 @@ spawn(function()
 		Name = "Radius",
 		Min = 0,
 		Max = 28,
-		Default = 14,
+		Default = 12,
 		Callback = function(callback)
 			if callback then
 				Radius = callback
@@ -555,16 +570,6 @@ spawn(function()
 				for _,bodypart in pairs(LocalPlayer.Character:GetChildren()) do
 					if bodypart:IsA("MeshPart") then
 						bodypart.BrickColor = BrickColor.Random()
-						for _, accessories in pairs(LocalPlayer.Character:GetChildren()) do
-							if accessories:IsA("Accessory") then
-								accessories:Destroy()
-								for _,clothes in pairs(LocalPlayer.Character:GetChildren()) do
-									if clothes:IsA("Shirt") or clothes:IsA("Pants") then
-										clothes:Destroy()
-									end
-								end
-							end
-						end
 					end
 				end
 			else
