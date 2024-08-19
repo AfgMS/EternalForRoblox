a9a58efd13495458141982a404d35aa552c92c3a
nothm_ <127741557+AfgMS@users.noreply.github.com>
2024-08-19 18:15:40 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -7,19 +7,9 @@ local Library = "https://raw.githubusercontent.com/AfgMS/EternalForRoblox/main/L
 local BridgeDuels = "https://raw.githubusercontent.com/AfgMS/EternalForRoblox/main/BridgeDuels.lua"
 local Universal = "https://raw.githubusercontent.com/AfgMS/EternalForRoblox/main/Universal.lua"
 
-local function GetGithub(url, path)
-	local content = game:HttpGet(url)
-	if content then
-		writefile(path, content)
-	else
-		writefile(LogsFolder .. "/update_error.txt", "Unable to update Eternal. Dm nothm_ on discord for support")
-	end
-end
-
 if not isfolder(MainFolder) then makefolder(MainFolder) end
 if not isfolder(ConfigFolder) then makefolder(ConfigFolder) end
 if not isfolder(LogsFolder) then makefolder(LogsFolder) end
-if not isfile(MainFolder .. "/library.txt") then GetGithub(Library, MainFolder .. "/library.txt") end
 
 if isfolder(MainFolder) and isfolder(ConfigFolder) and isfolder(LogsFolder) then
 	if game.PlaceId == 6872274481 or game.PlaceId == 8560631822 or game.PlaceId == 8444591321 then
